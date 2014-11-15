bf19c23d2cf9468808770bd96128d0bcdce39af4
TheoWinter <theo.winter@gmail.com>
2014-11-15 14:38:16 +0100

    the chat can now respond to a simple regex query

--- DawnChat/app.js
+++ DawnChat/app.js
@@ -7,19 +7,34 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+var parseMessage = function (msg) {
+    "use strict";
+    if (/^Hello/im.test(msg)) {
+        return "Hello to you too";
+    }
+    return null;
+};
+
 io.on('connection', function (socket) {
     "use strict";
     console.log('a user connected');
-    io.emit('a user connected');
+    io.emit('chat message', 'a user connected');
 
     socket.on('disconnect', function () {
         console.log('user disconnected');
-        io.emit('a user disconnected');
+        io.emit('chat message', 'a user disconnected');
     });
 
     socket.on('chat message', function (msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        if (msg.length > 0) {
+            console.log('message: ' + msg);
+            io.emit('chat message', msg);
+
+            var botResponse = parseMessage(msg);
+            if (botResponse !== null) {
+                io.emit('chat message', "Bot: " + botResponse);
+            }
+        }
     });
 });
 
