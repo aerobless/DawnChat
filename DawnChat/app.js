d32f0738ef3bd4ee7aff704fe7c816f32a4ef730
TheoWinter <theo.winter@gmail.com>
2014-11-15 13:17:31 +0100

    cleaned up server.js to comply better with JSLint

--- DawnChat/app.js
+++ DawnChat/app.js
@@ -3,20 +3,27 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+    "use strict";
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+    "use strict";
+    console.log('a user connected');
+    io.emit('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+        io.emit('a user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function () {
-  console.log('listening on *:3000');
+    "use strict";
+    console.log('listening on *:3000');
 });
